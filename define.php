1c11275f342e9dead1c2cee13cf49e749bb18787
takuma2xyx <50941426+takuma2xyx@users.noreply.github.com>
2019-05-28 17:44:06 +0900

    Add files via upload

---
+++ define.php
@@ -0,0 +1,4 @@
+<?php
+define('os03bn/bYez4xcqUJ4WzlnoIceFt2UJ9cRAoVFonPrOFvy6p2ymTsK+/XdUKNVionLLz7L8fGWaZeXN/KN0fKAy9wruQxbdRTvT9fgJ2BG9fizhO1jsD6ulGohJc/oZodz9tsLCQ5l5f9Rg+G+rD4wdB04t89/1O/w1cDnyilFU=');  //アクセストークン
+define('CHANNEL_SECRET','abeabc699398d3890527747f6da6a523');  //シークレット
+?>
